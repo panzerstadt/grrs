17793f4a9930d79523f55131222f8059e71bf19a
panzerstadt <tliqun@gmail.com>
2022-10-17 18:24:38 +0900

    clap version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-// use clap::Parser;
+use clap::Parser;
 
-// /// Search for a pattern in a file and display the lines that contain it.
-// #[derive(Parser)]
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
     /// The pattern to look for
     pattern: String,
@@ -12,12 +12,7 @@ struct Cli {
 fn main() {
     println!("Hello, world!");
 
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
     println!("argssssss {}", args.pattern);
     println!("path {}", args.path.display());
